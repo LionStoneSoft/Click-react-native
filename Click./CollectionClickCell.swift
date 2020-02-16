3724970164836f9ddc8c31027162da9a808f4aa9
Ryan Soanes <ryan@lionstone.app>
2020-02-16 14:14:11 +0000

    Collection View basics work, no constraints

---
+++ Click./CollectionClickCell.swift
@@ -0,0 +1,21 @@
+//
+//  CollectionClickCell.swift
+//  Click.
+//
+//  Created by Ryan Soanes on 16/02/2020.
+//  Copyright © 2020 LionStone. All rights reserved.
+//
+
+import UIKit
+
+class CollectionClickCell: UICollectionViewCell {
+
+    @IBOutlet var cellBackgroundView: UIView!
+    @IBOutlet var cellNameLabel: UILabel!
+    @IBOutlet var cellLastDateAndTime: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
--- Click./ViewController.swift
+++ Click./ViewController.swift
@@ -8,13 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
 
+    @IBOutlet var clickCollection: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        clickCollection.delegate = self
+        clickCollection.dataSource = self
+        clickCollection.register(UINib(nibName: "CollectionClickCell", bundle: nil), forCellWithReuseIdentifier: "collectionClickCell")
+
     }
 
-
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "collectionClickCell", for: indexPath as IndexPath) as! CollectionClickCell
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        // handle tap events
+        print("You selected cell #\(indexPath.item)!")
+    }
+    
 }
 
